9bdc7fcae69d1b0644c870fdb9dfce006d3c144e
Murray Resinski <murray.resinski@octanner.com>
2019-07-13 17:36:49 -0600

    set up to crash

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 	"os"
 )
-
+var total int
 func main() {
-
+        total = 0
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
                 fmt.Printf("value of environment variable %v: %v\n", "ECHO_VAR", os.Getenv("ECHO_VAR"))
+                total++
 		r.Write(w)
+                if total > 9 {
+                    os.Exit(2)
+                }
 	})
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
